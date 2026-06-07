1826725e0fca95bc29ddf49cec6a184a37c5117c
KirayaaTeam <kirayaateam@gmail.com>
2026-06-07 22:32:10 +0530

    Update MainActivity.java

--- exp1/MainActivity.java
+++ exp1/MainActivity.java
@@ -1,14 +1,87 @@
-package com.example.myapplication;
+package com.example.arithmeticapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
+
+    EditText num1, num2;
+    Button btnAdd, btnSub, btnMul, btnDiv;
+    TextView result;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        num1 = findViewById(R.id.num1);
+        num2 = findViewById(R.id.num2);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        btnSub = findViewById(R.id.btnSub);
+        btnMul = findViewById(R.id.btnMul);
+        btnDiv = findViewById(R.id.btnDiv);
+
+        result = findViewById(R.id.result);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                double a = Double.parseDouble(num1.getText().toString());
+                double b = Double.parseDouble(num2.getText().toString());
+
+                double res = a + b;
+
+                result.setText("Addition = " + res);
+            }
+        });
+
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                double a = Double.parseDouble(num1.getText().toString());
+                double b = Double.parseDouble(num2.getText().toString());
+
+                double res = a - b;
+
+                result.setText("Subtraction = " + res);
+            }
+        });
+
+        btnMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                double a = Double.parseDouble(num1.getText().toString());
+                double b = Double.parseDouble(num2.getText().toString());
+
+                double res = a * b;
+
+                result.setText("Multiplication = " + res);
+            }
+        });
+
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                double a = Double.parseDouble(num1.getText().toString());
+                double b = Double.parseDouble(num2.getText().toString());
+
+                if (b != 0) {
+                    double res = a / b;
+                    result.setText("Division = " + res);
+                } else {
+                    result.setText("Cannot divide by zero");
+                }
+            }
+        });
     }
 }
